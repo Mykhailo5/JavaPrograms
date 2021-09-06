8af5515e9c2b97457f01e3191ad9cdf26bf60f2d
Mykhailo5 <mykhailomykhailyshyn@gmail.com>
2021-09-06 14:51:46 +0300

    *two numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,11 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-       System.out.println(doubleInteger(3));
+        System.out.println(multiply(2, 3));
     }
+    public static int multiply(int num1, int num2) {
 
-    public static int doubleInteger(int i) {
-        return i * 2 ;
+        return num1*num2;
     }
 }
